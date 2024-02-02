319d67d4d4ef0bc440516ddbd150cca892e87cf1
Jingxiang Mo <jingxiangmo@gmail.com>
2024-02-02 16:47:30 +0800

    Fixed left right

--- fpv_car/fpv_car.ino
+++ fpv_car/fpv_car.ino
@@ -1,3 +1,4 @@
+#include <LiquidCrystal_I2C.h>
 #include <PS2X_lib.h>
 
 // ----- front Motors ----- //
@@ -125,44 +126,36 @@ void moveStop(){
 }
 
 void turnLeft() {
-  // Slow down or reverse left motors
-  digitalWrite(in1, HIGH); // Reverse direction or LOW to slow down
-  digitalWrite(in2, LOW);  // Reverse direction or HIGH to slow down
-  analogWrite(enA, 128);   // Adjust speed as necessary
-
-  digitalWrite(in5, HIGH); // Reverse direction or LOW to slow down
-  digitalWrite(in6, LOW);  // Reverse direction or HIGH to slow down
-  analogWrite(enC, 128);   // Adjust speed as necessary
-
-  // Keep right motors moving forward
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  analogWrite(enB, 255);
-
-  digitalWrite(in7, LOW);
-  digitalWrite(in8, HIGH);
-  analogWrite(enD, 255);
-}
-
-
-void turnRight() {
-  // Slow down or reverse right motors
-  digitalWrite(in3, HIGH); // Reverse direction or LOW to slow down
-  digitalWrite(in4, LOW);  // Reverse direction or HIGH to slow down
-  analogWrite(enB, 128);   // Adjust speed as necessary
-
-  digitalWrite(in7, HIGH); // Reverse direction or LOW to slow down
-  digitalWrite(in8, LOW);  // Reverse direction or HIGH to slow down
-  analogWrite(enD, 128);   // Adjust speed as necessary
-
-  // Keep left motors moving forward
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   analogWrite(enA, 255);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enB, 0);
 
   digitalWrite(in5, LOW);
   digitalWrite(in6, HIGH);
   analogWrite(enC, 255);
+  digitalWrite(in7, LOW);
+  digitalWrite(in8, HIGH);
+  analogWrite(enD, 125);
+}
+
+
+void turnRight() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enA, 0);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enB, 255);
+
+  digitalWrite(in5, LOW);
+  digitalWrite(in6, HIGH);
+  analogWrite(enC, 125);
+  digitalWrite(in7, LOW);
+  digitalWrite(in8, HIGH);
+  analogWrite(enD, 255);
 }
 
 
@@ -171,45 +164,7 @@ void ps2Controller(){
     return; 
   }
   
- if(type == 2){
-   
-   ps2x.read_gamepad();          //read controller 
-   
-   if(ps2x.ButtonPressed(GREEN_FRET))
-     Serial.println("Green Fret Pressed");
-   if(ps2x.ButtonPressed(RED_FRET))
-     Serial.println("Red Fret Pressed");
-   if(ps2x.ButtonPressed(YELLOW_FRET))
-     Serial.println("Yellow Fret Pressed");
-   if(ps2x.ButtonPressed(BLUE_FRET))
-     Serial.println("Blue Fret Pressed");
-   if(ps2x.ButtonPressed(ORANGE_FRET))
-     Serial.println("Orange Fret Pressed");
-     
-
-    if(ps2x.ButtonPressed(STAR_POWER))
-     Serial.println("Star Power Command");
-    
-    if(ps2x.Button(UP_STRUM))          //will be TRUE as long as button is pressed
-     Serial.println("Up Strum");
-    if(ps2x.Button(DOWN_STRUM))
-     Serial.println("DOWN Strum");
-  
- 
-    if(ps2x.Button(PSB_START))                   //will be TRUE as long as button is pressed
-         Serial.println("Start is being held");
-    if(ps2x.Button(PSB_SELECT))
-         Serial.println("Select is being held");
-    
-    if(ps2x.Button(ORANGE_FRET)) // print stick value IF TRUE
-    {
-        Serial.print("Wammy Bar Position:");
-        Serial.println(ps2x.Analog(WHAMMY_BAR), DEC); 
-    } 
- }
-
- else { //DualShock Controller
-  
+ if(type != 2){
     ps2x.read_gamepad(false, vibrate);          //read controller and set large motor to spin at 'vibrate' speed
 
     if(ps2x.ButtonPressed(PSB_GREEN)){
@@ -228,14 +183,11 @@ void ps2Controller(){
       Serial.println("Circle just pressed. MOVE RIGHT.");
       moveStop();
     }
-
  }
 
 }
 
 void loop() {
-  // Serial.print("test");
-
   ps2Controller();
   delay(50);
 }
